564508778cfba23010e27a7576e47f8ce4251f92
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2023-10-20 00:54:49 +0600

    Get and Post Method created for Add to Cart

--- index.js
+++ index.js
@@ -17,45 +17,58 @@ const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
-  serverApi: {
-    version: ServerApiVersion.v1,
-    strict: true,
-    deprecationErrors: true,
-  }
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
 });
 
 async function run() {
-  try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    try {
+        // Connect the client to the server	(optional starting in v4.7)
+        await client.connect();
 
-    // Product Collection
-    const productCollection = client.db('productsDb').collection('products')
-
-
-    // Add a Product
-    app.post('/products',async(req,res)=>{
-        const newProduct = req.body
-        const result = await productCollection.insertOne(newProduct)
-        res.send(result)
-    })
-
-    // Get Product
-    app.get('/products',async(req,res)=>{
-        const cursor = productCollection.find()
-        const result = await cursor.toArray()
-        res.send(result)
-    })
+        // Product Collection
+        const productCollection = client.db('productsDb').collection('products')
+        const cartCollection = client.db('productsDb').collection('cart')
 
 
+        // Add a Product
+        app.post('/products', async (req, res) => {
+            const newProduct = req.body
+            const result = await productCollection.insertOne(newProduct)
+            res.send(result)
+        })
 
-    // Send a ping to confirm a successful connection
-    await client.db("admin").command({ ping: 1 });
-    console.log("Pinged your deployment. You successfully connected to MongoDB!");
-  } finally {
-    // Ensures that the client will close when you finish/error
-    // await client.close();
-  }
+        // Get Product
+        app.get('/products', async (req, res) => {
+            const cursor = productCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+       app.post('/cart',async(req,res)=>{
+       const viewDetails= req.body
+       const result = await cartCollection.insertOne(viewDetails)
+       res.send(result)
+       })
+
+        app.get('/cart', async (req, res) => {
+            const cursor = cartCollection.find()
+            const result = await cursor.toArray()
+            res.send(result)
+        })
+
+
+
+        // Send a ping to confirm a successful connection
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // Ensures that the client will close when you finish/error
+        // await client.close();
+    }
 }
 run().catch(console.dir);
 
@@ -65,5 +78,5 @@ app.get('/', (req, res) => {
 })
 
 app.listen(port, () => {
-console.log(`FashionFlare server is running on port : ${port}`);
+    console.log(`FashionFlare server is running on port : ${port}`);
 })
