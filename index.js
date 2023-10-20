9af51e52928fb9dbf61040f4bb0639b72eccd535
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2023-10-20 20:14:19 +0600

    Al the method edited and vercel json added

--- index.js
+++ index.js
@@ -93,7 +93,7 @@ async function run() {
         // Delete From Cart
         app.delete('/cart/:id', async (req, res) => {
             const id = req.params.id
-            const query = { _id: id }
+            const query = { _id: new ObjectId(id) }
             const result = await cartCollection.deleteOne(query)
             res.send(result)
         })
