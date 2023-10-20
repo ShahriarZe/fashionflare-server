64b95875c0c2eb31b23afd99046b1210bb434b50
Shahriar Ahmmed <shahriarzemam28@gmail.com>
2023-10-20 12:06:18 +0600

    Deleted Method completed

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
@@ -48,15 +48,22 @@ async function run() {
             res.send(result)
         })
 
-       app.post('/cart',async(req,res)=>{
-       const viewDetails= req.body
-       const result = await cartCollection.insertOne(viewDetails)
-       res.send(result)
-       })
+        app.post('/cart', async (req, res) => {
+            const viewDetails = req.body
+            const result = await cartCollection.insertOne(viewDetails)
+            res.send(result)
+        })
 
         app.get('/cart', async (req, res) => {
             const cursor = cartCollection.find()
             const result = await cursor.toArray()
+            res.send(result)
+        })
+
+        app.delete('/cart/:id', async (req, res) => {
+            const id = req.params.id
+            const query = {_id : new ObjectId(id)}
+            const result =await cartCollection.deleteOne(query)
             res.send(result)
         })
 
